b017f12251a5217b4ac2a6db7a58a7df0434fffb
it12136 <it12136@uom.edu.gr>
2017-01-27 13:33:50 +0200

    back-up

---
+++ app/src/main/java/com/example/user/skoufisjan17/Merchant.java
@@ -0,0 +1,35 @@
+package com.example.user.skoufisjan17;
+
+public class Merchant {
+
+    private String id;
+    private String legalName;
+    private String category;
+    private String address;
+    private String imageUrl;
+    private String review;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getLegalName() {
+        return legalName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getReview() {
+        return review;
+    }
+}
